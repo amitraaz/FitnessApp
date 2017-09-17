3f03887daf2b5019e4dcfde3359bf70fe9f4e4bb
Amit Mishra <amitraaz@gmail.com>
2017-09-18 00:13:41 +0530

    first commit

---
+++ Fitness/Fitness.API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using APM.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Fitness.API.Controllers
+{
+  //  [EnableCorsAttribute("*", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Product> Get()
+        {
+            ProductRepository rep = new ProductRepository();
+            return rep.Retrieve();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
